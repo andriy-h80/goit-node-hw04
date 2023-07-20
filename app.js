2305260f0f97455d598f472e2a437e679dc81d43
andriy-h80 <andrey.hanzel@gmail.com>
2023-07-20 10:55:05 +0300

    hw #04 done

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/users', userRouter);
+app.use('/users', userRouter);
 app.use('/api/contacts', contactsRouter);
 
 app.use((req, res) => {
--- controllers/users.js
+++ controllers/users.js
@@ -66,10 +66,8 @@ const getCurrent = async(req, res) => {
     const {email, subscription} = req.user;
 
     res.status(200).json({
-        "user": {
-            email,
-            subscription,
-        }
+        email,
+        subscription,
     }); 
 };
 
